6af0a0e29d6107a6bffae8280d1cabad5b00d2cb
Ver0-nika <52762686+Ver0-nika@users.noreply.github.com>
2019-08-05 23:38:36 +0200

    Add files via upload

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Profile;
+
+class UserController extends Controller
+{
+    public function index(){
+    	return view('profile.index');
+    }
+
+    public function store(Request $request){
+    	//$request->get('address');
+    	//request('address');
+    	$user_id = auth()->user()->id;
+    	Profile::where('user_id',$user_id)->update([
+    		'address'=>request('address'),
+    		'experience'=>request('experience'),
+    		'bio'=>request('bio')
+    	]);
+    	return redirect()->back()->with('message','Profile Sucessfully Updated !');
+    }
+}
